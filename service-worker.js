2eb4ccecda15901e98d3a866c8f5d71e73018382
Erik Figueiredo <erik.figueiredo@gmail.com>
2017-09-07 19:58:31 -0300

    first commit

---
+++ service-worker.js
@@ -0,0 +1,68 @@
+var cacheName = 'notes-son.v1.0.0';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/css/colors.css',
+    '/css/styles.css',
+    '/js/array.observe.polyfill.js',
+    '/js/object.observe.polyfill.js',
+    '/js/scripts.js',
+    '/service-worker.js',
+    'http://pokeapi.co/api/v2/evolution-chain/',
+    'https://unpkg.com/axios/dist/axios.min.js',
+    '/manifest.json',
+    '/img/icons/icon-128x128.png',
+    '/img/icons/icon-144x144.png',
+    '/img/icons/icon-152x152.png',
+    '/img/icons/icon-192x192.png',
+    '/img/icons/icon-256x256.png',
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+function promiseAny(promises) {
+  return new Promise((resolve, reject) => {
+    promises = promises.map(p => Promise.resolve(p));
+    promises.forEach(p => p.then(resolve));
+    promises.reduce((a, b) => a.catch(() => b))
+      .catch(() => reject(Error("All failed")));
+  });
+};
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+  );
+  /*e.respondWith(
+    promiseAny([
+      caches.match(e.request),
+      fetch(e.request)
+    ])
+  );*/
+});
